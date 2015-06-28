165fd4baadc1da65865b35ad74e9a384de6dba6e
Vladas Zakrevskis <146100@gmail.com>
2015-06-28 03:05:50 +0300

    ...

--- Методичка/Урок 28 - База данных гаи/Урок 28 - База данных гаи/Source.cpp
+++ Методичка/Урок 28 - База данных гаи/Урок 28 - База данных гаи/Source.cpp
@@ -15,9 +15,10 @@ struct Base {
 		string violation[10];
 
 		// поля для создания дерева
+		
 		Node* left;
 		Node* right;
-
+		Node(){}
 		Node(int n, string violation) : n(n), left(nullptr), right(nullptr)
 		{
 			this->violation[0] = violation;
@@ -29,39 +30,97 @@ struct Base {
 
 		}
 		void show() {
-			cout << "Номер машины: " << n << endl;
-			cout << "Нарушения: " << endl;
 
-			for (int i = 0; i <=violationCount;i++) {
-				cout << violation[i] << endl;
+			if (n == -1) cout << "Номер не найден" << endl;
+			else {
+				cout << "Номер машины: " << n << endl;
+				cout << "Нарушения: " << endl;
+
+				for (int i = 0; i <= violationCount;i++) {
+					cout << violation[i] << endl;
+				}
+				cout << endl;
 			}
-			cout << endl;
 		}
-		
 	};
 
+	size_t size = 0;
 	Node* root = nullptr;
 
-	
+	Node* sorted;
+	Base() {
+		for (int i = 0; i < 20;i++) {
+			addPenalty(generateCar());
+		}
+	}
+
+
+	void addToSort(Node* node) {
+		static int i = 0;
+		if (node != nullptr) {
+			addToSort(node->left);
+			sorted[i] = *node;
+			i++;
+			addToSort(node->right);
+		}
+
+	}
+
+	void sort() {
+		sorted = new Node[size];
+		addToSort(root);
+	}
+
+	void showSort() {
+		for (int i = 0;i < size;i++) {
+			cout << sorted[i].n << endl;
+		}
+	}
+	Node* generateCar() {
+		int n = rand() % 10000;
+		
+		string viol[10] = { "Превышение скорости",
+		"Проезд на красный свет",
+		"Парковка в неположенном месте",
+			"Проезд через 2 сплошные",
+			"Обгон в неположенном месте" ,
+			"Создание аварийной ситуации" ,
+			"Управление в состоянии алкогольного опьянения" ,
+			"Нарушение правил буксировки" ,
+			"Стоянка в неположенном месте" ,
+			"Не пристегнут ремень" };
+
+
+		Node* car = new Node(n, viol[rand() % 10]);
+
+		int violCount = rand() % 4;
+
+		for (int i=0;i < violCount;i++) {
+			car->addViolation(viol[rand() % 10]);
+		}
+
+		return car;
+	}
 
 	void addPenalty(int n, string violation) {
 		Node* newNode = new Node(n, violation);
 		if (root == nullptr) 
 		{
 			root = newNode;
-			
+			size++;
 		}
 		else {
 
 			Node* current = root;
 			Node* prev = current;
+			
+			
 			int compare = 0;
 
 			while (current != nullptr) {
-
+			
 				prev = current;
-
-				if (current->n = n)
+				if (current->n == n)
 				{
 					current->addViolation(violation);
 				}
@@ -91,13 +150,107 @@ struct Base {
 			else {
 				prev->left = current;
 			}
+			size++;
 		}
+	}
+	void addPenalty(Node* car) {
+		Node* newNode = car;
+		if (root == nullptr)
+		{
+			root = newNode;
+			size++;
+
+		}
+		else {
+
+			Node* current = root;
+			Node* prev = current;
+
+
+			int compare = 0;
+
+			while (current != nullptr) {
+
+				prev = current;
+				
+				if (current->n == car->n)
+				{
+					current = car;
+				}
+
+
+
+
+				if (current->n < car->n)
+				{
+					compare = MORE;
+					current = current->right;
+				}
+
+				else
+				{
+					compare = LESS;
+					current = current->left;
+				}
+
+			}
+
+			current = newNode;
+
+			if (compare == MORE) {
+				prev->right = current;
+			}
+			else {
+				prev->left = current;
+			}
+			size++;
+		}
+	}
+	void show(Node* node) {
+		if (node != nullptr) {
+			show(node->left);
+			node->show();
+			show(node->right);
+		}
+
+	}
+	Node& operator [](int name) {
+		Node* current = root;
+
+		while (current->n != name) {
+
+			if (current->n < name) {
+				current = current->right;
+			}
+			else {
+				current = current->left;
+			}
+			if (current == nullptr) {
+				
+				current = new Node(-1, " ");
+				return *current;
+			}
+		}
+		
+		return *current;
+
+
 	}
 
 
+	void showInRange(int from, int to) {
+		Node* current = root;
+		
+		
 
-
-
+		while (current->right->n < from) {
+			cout << current->n << endl;
+			if (current->n < from) current = current->right;
+			else current = current->left;
+		}
+		cout << endl;
+		cout << current->n << endl;
+	}
 
 
 };
@@ -107,16 +260,17 @@ int main() {
 
 	Base a;
 	
-	a.addPenalty(4565, "Дурны");
-	a.addPenalty(3333, "Нямы");
-	a.addPenalty(2222, "Сляпы");
-	a.addPenalty(1111, "Сляпы");
-	a.addPenalty(1234, "Сляпы");
+
+
+	
 
 
 
-	a.root->show();
-
-
+	a.show(a.root);
+	a[1234].show();
+	a.sort();
+	a.showSort();
+	cout << a.size << endl;
+	//a.showInRange(4000, 0);
 	return 0;
 }
