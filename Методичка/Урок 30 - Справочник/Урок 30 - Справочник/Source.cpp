c39045c44131fdda98f88625bd7b2bd3ee0d9027
Vladas Zakrevskis <146100@gmail.com>
2015-07-01 23:46:58 +0300

    Урок 30 - генератор имен

---
+++ Методичка/Урок 30 - Справочник/Урок 30 - Справочник/Source.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Company {
+	string name;
+	string owner;
+	int phone;
+	string line;
+
+
+
+	Company generateCompany() {
+		
+		string brandParts[10] = {
+			"Pro", "Top", "Elite", "Great", "Super", "Rich",
+			"Ultra", "Business", "Company", "Concern"
+		};
+		string names[20] = {
+				"Jacob"	,
+				"Michael",
+				"Joshua	",
+				"Matthew",
+				"Ethan"	,
+				"Andrew",
+				"Daniel",
+				"William",
+				"Joseph"	,
+				"Christopher",
+				"Anthony"	,
+				"Ryan"	,
+				"Nicholas",
+				"David"	,
+				"Alexander"	,
+				"Tyler"	,
+				"James"	,
+				"John"	,
+				"Dylan"	,
+				"Nathan"
+		};
+
+		string lastNames[20]{
+
+			"Abramson"	,	"Hoggarth"	,
+			"Adamson"	,	"Holiday"	,
+			"Adderiy"	,	"Holmes"	,
+			"Addington"	,	"Howard"	,
+			"Adrian"	,	"Jacobson"	,
+			"Albertson"	,	"James"	,
+			"Aldridge"	,	"Jeff"	,
+			"Allford"	,		"Barnes",
+			"Alsopp"	,		"Bishop",
+			"Anderson"	,		
+			"Andrews"			
+		
+
+		};
+
+
+		
+	}
+
+};
+
+struct Catalog {
+	
+
+	
+
+
+
+
+};
+
+int main() {
+
+
+
+
+
+}
