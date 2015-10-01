e3947bea0aeca8248ca20a6c11f3e4a989e41c09
Vladas Zakrevskis <146100@gmail.com>
2015-10-01 23:31:57 +0400

    3.1

--- WinApi/3/1 - Creating statics/1 - Creating statics/Source.cpp
+++ WinApi/3/1 - Creating statics/1 - Creating statics/Source.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+int i = 0;
+int j = 0;
 #define LAST_STATIC statics[statics.size() - 1]
 
 BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
@@ -21,6 +23,8 @@ struct staticRect {
 	int height;
 
 	staticRect(HWND hwnd, int x, int y, int width, int height) : handle(hwnd), x(x), y(y), width(width), height(height) {}
+
+	
 };
 
 vector<shared_ptr<staticRect>> statics;
@@ -52,7 +56,7 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		
 
 
-		infoButton = GetDlgItem(hWnd, IDC_BUTTON1);
+		infoButton = GetDlgItem(hWnd, IDC_BUTTON1);// getting descriptor of the info button
 		wsprintf(infoText, TEXT("Arrow: X=%d  Y=%d"), LOWORD(lParam), HIWORD(lParam)); 
 		SetWindowText(infoButton, infoText);	
 
@@ -68,7 +72,7 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		{
 
 			
-			MoveWindow(
+			MoveWindow(// changing size of new static when mouse is moving
 				LAST_STATIC->handle,
 				LAST_STATIC->x,
 				LAST_STATIC->y,
@@ -97,7 +101,7 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		statics.push_back
 			( make_shared<staticRect>(
 				CreateWindowEx(0, TEXT("STATIC"), 0,
-					WS_CHILD | WS_VISIBLE | WS_BORDER | SS_CENTER | WS_EX_CLIENTEDGE,
+					WS_CHILD |  WS_VISIBLE | WS_BORDER | SS_CENTER | WS_EX_CLIENTEDGE,
 					LOWORD(lParam), HIWORD(lParam), 0, 0, hWnd, 0, hInst, 0), 
 				LOWORD(lParam),
 				HIWORD(lParam),
@@ -113,15 +117,51 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_LBUTTONUP:
 			mouseDown = false;
 
-			if (LAST_STATIC->height < 10 && LAST_STATIC->width < 10) {// destroy static if it's less then 10*10
+			if (LAST_STATIC->height < 10 && LAST_STATIC->width < 10 || LAST_STATIC->height < 0 || LAST_STATIC->height < 0) {// destroy static if it's less then 10*10
 				DestroyWindow(LAST_STATIC->handle);
 				statics.pop_back();
 			}
 
 
 		return TRUE;
+
+
+
+
 	case WM_RBUTTONDOWN:
+
+		i = 0;
+		for (auto a : statics) {++i;
+			
+			
+			if (LOWORD(lParam) > a->x && LOWORD(lParam) < a->x + a->height
+				&& HIWORD(lParam) > a->y && HIWORD(lParam) < a->y + a->width) {
+
+				wsprintf(infoText, TEXT("#%d, HIGHT = %d, WIDTH = %d, X position = %d, Y position = %d"), i, a->height, a->width, a->x, a->y);
+				SetWindowText(hWnd, infoText);
+
+				
+			}
+		}
 		
+		return TRUE;
+
+	case WM_LBUTTONDBLCLK:
+
+		j = 0;
+		for (auto a : statics) {
+			++j;
+
+
+			if (LOWORD(lParam) > a->x && LOWORD(lParam) < a->x + a->height
+				&& HIWORD(lParam) > a->y && HIWORD(lParam) < a->y + a->width) {
+
+				DestroyWindow(a->handle);
+				statics.erase(statics.begin()+j);
+				break;
+
+			}
+		}
 		return TRUE;
 	}
 	return FALSE;
