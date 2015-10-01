268949892ac7d9b2f25107c07a2d46492fcdf6f3
Vladas Zakrevskis <146100@gmail.com>
2015-10-02 00:53:54 +0300

    Update Source.cpp

--- WinApi/3/1 - Creating statics/1 - Creating statics/Source.cpp
+++ WinApi/3/1 - Creating statics/1 - Creating statics/Source.cpp
@@ -48,30 +48,30 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	switch (message)
 	{
 	case WM_CLOSE:
-		EndDialog(hWnd, 0); 
+		EndDialog(hWnd, 0);
 		return TRUE;
-	
+
 
 	case WM_INITDIALOG:
-		
+
 
 
 		infoButton = GetDlgItem(hWnd, IDC_BUTTON1);// getting descriptor of the info button
-		wsprintf(infoText, TEXT("Arrow: X=%d  Y=%d"), LOWORD(lParam), HIWORD(lParam)); 
-		SetWindowText(infoButton, infoText);	
+		wsprintf(infoText, TEXT("Arrow: X=%d  Y=%d"), LOWORD(lParam), HIWORD(lParam));
+		SetWindowText(infoButton, infoText);
 
 		return TRUE;
 
 
 	case WM_MOUSEMOVE:
-		wsprintf(infoText, TEXT("Arrow: X=%d  Y=%d"), LOWORD(lParam), HIWORD(lParam)); // mouse coordinates for info
-		SetWindowText(infoButton, infoText);	
+		wsprintf(infoText, TEXT("Arrow: X=%d  Y=%d size= %d"), LOWORD(lParam), HIWORD(lParam), statics.size()); // mouse coordinates for info
+		SetWindowText(infoButton, infoText);
 
 
 		if (mouseDown)
 		{
 
-			
+
 			MoveWindow(// changing size of new static when mouse is moving
 				LAST_STATIC->handle,
 				LAST_STATIC->x,
@@ -96,6 +96,7 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 				wsprintf(infoText, TEXT("#%d, HIGHT = %d, WIDTH = %d, X position = %d, Y position = %d"), i, stat->height, stat->width, stat->x, stat->y);
 				SetWindowText(hWnd, infoText);
+				
 
 				break;
 			}
@@ -118,22 +119,31 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		mouseDown = true;
 
 		statics.push_back
-			( make_shared<staticRect>(
+			(make_shared<staticRect>(
 				CreateWindowEx(0, TEXT("STATIC"), 0,
-					WS_CHILD |  WS_VISIBLE | WS_BORDER | SS_CENTER | WS_EX_CLIENTEDGE,
-					LOWORD(lParam), HIWORD(lParam), 0, 0, hWnd, 0, hInst, 0), 
+					WS_CHILD | WS_VISIBLE | WS_BORDER | SS_CENTER | WS_EX_CLIENTEDGE,
+					LOWORD(lParam), HIWORD(lParam), 0, 0, hWnd, 0, hInst, 0),
 				LOWORD(lParam),
 				HIWORD(lParam),
-				0,0)
+				0, 0)
 				);
 
-	
+
 
 
 
 		return TRUE;
 
 	case WM_LBUTTONUP:
+
+		mouseDown = false;
+		if (statics.size()) {
+			if (LAST_STATIC->height < 10 && LAST_STATIC->width < 10 || LAST_STATIC->height < 0 || LAST_STATIC->height < 0) {// destroy static if it's less then 10*10
+				DestroyWindow(LAST_STATIC->handle);
+				statics.pop_back();
+			}
+		}
+
 			mouseDown = false;
 			
 			if(statics.size()){
@@ -143,17 +153,18 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 				}
 			}
 
+
 		return TRUE;
 
 
 
 		//moved to WM_MOUSEMOVE
-	/*case WM_RBUTTONDOWN: 
+	/*case WM_RBUTTONDOWN:
 
-		
+
 		for (int i = 0; i < statics.size(); ++i) {
-			
-			
+
+
 			if (LOWORD(lParam) > stat->x && LOWORD(lParam) < stat->x + stat->height
 				&& HIWORD(lParam) > stat->y && HIWORD(lParam) < stat->y + stat->width) {
 
@@ -167,12 +178,13 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 				SetWindowText(hWnd, infoText);
 			}
 		}
-		
+
 		return TRUE;*/
 
 	case WM_LBUTTONDBLCLK:
-
 		
+
+		if (statics.size()) {
 		for (int i = 0; i < statics.size(); ++i) {
 
 
@@ -180,13 +192,15 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 				&& HIWORD(lParam) > stat->y && HIWORD(lParam) < stat->y + stat->width) {
 
 				DestroyWindow(stat->handle);
-				
-				if(i)statics.erase(statics.begin()+i);
-				else statics.pop_back();
+
+				statics.erase(statics.begin()+i);
+			
 				break;
 
 			}
 		}
+	}
+	
 		return TRUE;
 	}
 	return FALSE;
