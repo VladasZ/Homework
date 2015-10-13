96dcfd5a41210cef2e7185380de0657db1b53ed6
Vladas Zakrevskis <146100@gmail.com>
2015-10-13 23:49:20 +0400

    ...

--- WinApi/5/1 - Radio button/1 - Radio button/Source.cpp
+++ WinApi/5/1 - Radio button/1 - Radio button/Source.cpp
@@ -1,6 +1,6 @@
 #include <windows.h>
 #include "resource.h"
-
+#include <vector>
 
 
 using namespace std;
@@ -9,6 +9,11 @@ using namespace std;
 
 HINSTANCE hInst;
 HWND hList, hAdd, hMult, hMean, hDisplay, hButton;
+
+TCHAR text[50];
+
+vector<int> values;
+
 
 
 BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
@@ -23,6 +28,21 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpszCmdLine, 
 	return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc);
 }
 
+void generateValues() {
+	values.clear();
+
+	for (int i = 0; i < 10 + (rand() % 10); ++i) {
+
+		int n = 0;
+		while (!n) n = -10 + (rand() % 20);
+
+		values.push_back(n);
+
+		wsprintf(text, TEXT("%d"), n);
+		SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)text);
+	}
+
+}
 
 BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -43,14 +63,64 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		hDisplay = GetDlgItem(hWnd, DISPLAY);
 		hButton = GetDlgItem(hWnd, BUTTON1);
 
-
-
-
+		generateValues();
 
 		return TRUE;
 
 	case WM_COMMAND:
-		if (wParam == BUTTON1) //MessageBox(hWnd, TEXT("kk"), L"zaza", MB_OK);
+		if (wParam == BUTTON1) { //MessageBox(hWnd, TEXT("kk"), L"zaza", MB_OK);
+			SendMessage(hList, LB_RESETCONTENT, 0, 0);
+
+			generateValues();
+
+
+			if (IsDlgButtonChecked(hWnd, RADIO_ADDITION)) goto addition;
+			if (IsDlgButtonChecked(hWnd, RADIO_MULTIPLICATION)) goto addition;
+			if (IsDlgButtonChecked(hWnd, RADIO_MEAN)) goto addition;
+
+
+			return TRUE;
+		}
+
+		
+
+
+
+		if (LOWORD(wParam) == RADIO_ADDITION) {
+			addition:
+			int result = 0;
+
+			for (auto a : values) result += a;
+
+			wsprintf(text, L"%d", result);
+			SetWindowText(hDisplay, text);
+			return TRUE;
+		}
+
+		if (LOWORD(wParam) == RADIO_MULTIPLICATION) {
+			multiplication:
+			int result = 1;
+
+			for (auto a : values) result *= a;
+
+			wsprintf(text, L"%d", result);
+			SetWindowText(hDisplay, text);
+			return TRUE;
+		}
+
+
+		if (LOWORD(wParam) == RADIO_MEAN) {
+			mean:
+			int summ = 0;
+
+			for (auto a : values) summ += a;
+
+			int result = summ / (values.size());
+
+			wsprintf(text, L"%d", result);
+			SetWindowText(hDisplay, text);
+			return TRUE;
+		}
 
 
 	return TRUE;
