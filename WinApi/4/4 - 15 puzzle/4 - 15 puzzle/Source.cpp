d602bdefdf05918e43be20621f9b36e9ae60c146
Vladas Zakrevskis <146100@gmail.com>
2015-10-15 23:26:25 +0400

    15 puzzle beta

--- WinApi/4/4 - 15 puzzle/4 - 15 puzzle/Source.cpp
+++ WinApi/4/4 - 15 puzzle/4 - 15 puzzle/Source.cpp
@@ -7,7 +7,27 @@
 
 using namespace std;
 
-#define newH make_shared<HWND>
+#define ID_BUTTON1 1
+#define ID_BUTTON2 2
+#define ID_BUTTON3 3
+#define ID_BUTTON4 4
+#define ID_BUTTON5 5
+#define ID_BUTTON6 6
+#define ID_BUTTON7 7
+#define ID_BUTTON8 8
+#define ID_BUTTON9 9
+#define ID_BUTTON10 10
+#define ID_BUTTON11 11
+#define ID_BUTTON12 12
+#define ID_BUTTON13 13
+#define ID_BUTTON14 14
+#define ID_BUTTON15 15
+
+#define FOR_EACH_FIELD_ELEM for (int i = 1; i < 5; ++i) { for (int j = 1; j < 5; ++j) {if (i == 4 && j == 4) break;
+#define END_FOR_EACH }}
+#define ELEM field[j][i]
+
+#define EMPTY_ELEM game.field[game.emptyX][game.emptyY]
 
 HINSTANCE hInst;
 
@@ -15,21 +35,36 @@ TCHAR text[50];
 
 enum size { left, right, top, bot };
 
+struct Button {
+	int number;
+	HWND hWnd;
+	bool enable;
+
+};
+
+
+
 class Game {
 public:
-	int field[4][4] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };
-	HWND buttons[6][6];
+	Button field[6][6];
+
+	int emptyX=4, emptyY=4;
 
 
 
-	HWND activeButtons[4];
 
-	struct EmptyPosition {
-		int x;
-		int y;
-	}emptyPosition;
-
+	static void goToPos(HWND hWnd,int id, int x, int y) {
+		MoveWindow(GetDlgItem(hWnd, id),
+			x*50,
+			y*50,
+			50,
+			50,
+			1
+			);
 	
+	
+	}
+
 	void shuffle() {
 
 		for (int i = 0; i < 1000; ++i) {
@@ -37,81 +72,69 @@ public:
 		}
 	}
 
-	void setActiveButtons() {
 
-		bool brk = false;
-
-		for (int i = 1; i < 5; ++i) {
-			
-			for (int j = 1; j < 5; ++j) {
-
-				if (!buttons[j][i]) {
-
-					emptyPosition.x = j;
-					emptyPosition.y = i;
+	int setActiveButtons() {
 
 
-					activeButtons[top] = buttons[j - 1][i];
-					activeButtons[bot] = buttons[j + 1][i];
-					activeButtons[left] = buttons[j][i - 1];
-					activeButtons[right] = buttons[j][i + 1];
+		FOR_EACH_FIELD_ELEM
 
-					brk = true;
+				ELEM.enable = false; // resetting old values
 
-					break;
+		END_FOR_EACH
 
+
+
+
+			for (int i = 1; i < 5; ++i) {
+				for (int j = 1; j < 5; ++j) {
+
+					ELEM.enable = false;
+
+					if (!ELEM.hWnd) {
+						emptyX = j; emptyY = i;
+						field[j - 1][i].enable = true;
+						field[j + 1][i].enable = true;
+						field[j][i - 1].enable = true;
+						field[j][i + 1].enable = true;
+						return 1;
+					}
 
 				}
-
 			}
-			if (brk) break;
-		}
+
+		
+	
+		return 0;
 
 	}
 
-	
 
 	void init(HWND hWnd) {
 
-		int n = 1;
 
-		for (int i = 1; i < 5; ++i) {
-			for (int j = 1; j < 5; ++j) {
-				if (i == 4 && j == 4) break;
-				wsprintf(text, TEXT("%d"), n++);
+		int n = 0;
 
-				buttons[j][i] = CreateWindowEx(0, TEXT("BUTTON"), text,
+		FOR_EACH_FIELD_ELEM
+
+			if (j == 4 && i == 4) continue;
+				wsprintf(text, TEXT("%d"), ++n);
+
+			ELEM.hWnd = CreateWindowEx(0, TEXT("BUTTON"), text,
 					WS_CHILD | WS_VISIBLE | WS_BORDER | SS_CENTER | WS_EX_CLIENTEDGE,
-					j * 50, i * 50, 50, 50, hWnd, 0, hInst, 0);
+					j * 50, i * 50, 50, 50, hWnd, (HMENU)n, hInst, 0);
+			ELEM.number = n;
 
-			}
-		}
-	}
-
-	void tellActive(){
+			ELEM.enable = false;
 
 
-		for (int i = 0; i < 4; ++i) {
-			wsprintf(text, "%d %d", activeButtons[i])
-		}
+		END_FOR_EACH
+
 
 	}
 
-	void show() {
 
-	
 
-		for (int i = 1; i < 5; ++i) {
-			for (int j = 1; j < 5; ++j) {
-			
-				MoveWindow(buttons[j][i], j * 50, i * 50, 50, 50, 1);
 
-			
-			
-			}
-		}
-
-	}
 
 
 
@@ -144,11 +167,27 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_INITDIALOG:
 
 		game.init(hWnd);
-		
-		game.show();
+		game.setActiveButtons();
 
 		
+
 		return TRUE;
+
+	case WM_COMMAND:
+
+		
+		FOR_EACH_FIELD_ELEM
+
+				if (game.ELEM.number == wParam && game.ELEM.enable) {
+					Game::goToPos(hWnd, wParam, game.emptyX, game.emptyY);
+					swap(game.ELEM, EMPTY_ELEM);
+					game.setActiveButtons();
+					return TRUE;
+				}
+
+		END_FOR_EACH
+
+			return TRUE;
 	}
 
 
