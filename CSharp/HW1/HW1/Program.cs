13f8e37bc4b8df96038c16f34957d3fd8160fbb3
Vladas Zakrevskis <146100@gmail.com>
2015-10-31 19:12:38 +0300

    ...

---
+++ CSharp/HW1/HW1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Program
+    {
+
+        static bool N1(int a, int b)
+        {
+            if (a > b)
+            {
+                Console.WriteLine("Первое число должно быть меньше второго");
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
