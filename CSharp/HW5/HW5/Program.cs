4ee89cc53ef19946b860e09df388457cc17c8c47
Vladas Zakrevskis <146100@gmail.com>
2015-12-13 19:39:19 +0300

    Test

---
+++ CSharp/HW5/HW5/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace HW5
+{
+
+
+    class Events
+    {
+        public static void OnEvent(object sender, ref string status)
+        {
+            ((Car)sender).setStatus();
+            status += "\nМашина #" + ((Car)sender).n + " остановилась. Причина: \n";
+        }
+
+        public static void OnOverheat(object sender, ref string status)
+        {
+            status += "Перегрев двигателя";
+
+        }
+        public static void OnFuelEnd(object sender, ref string status)
+        {
+            status += "Кончилось топливо";
+        }
+        public static void OnFinish(object sender, ref string status)
+        {
+            status += "Машина доехала до финиша";
+        }
+
+
+    }
+
+    class Car
+    {
+
+        public delegate void eventHnd(object sender, ref string status);
+
+        public event eventHnd onEvent;
+       
+        #region СВОЙСТВА
+
+        static int N = 0; // переменные для нумерации автомобилей
+        public int n {private set; get;}
+
+        public int position { private set; get; } = 0;// координаты автомобиля относительно старта
+
+        string space = " ";// строка с пробелами, увеличивающаяся каждый ход машины для ее сдвига по экрану
+
+        public bool OK { private set; get; } = true; // поле указывающее на то, что машина едет и с ней все в порядке
+
+        int fuel = 500;
+        int consumption;
+        int temp = 200;
+
+        static Random rand = new Random();
+
+        string status = " ";
+        #endregion
+
+        public Car(int cons)// указываем расход топлива при создании автомобиля
+        {
+            n = N++;// нумеруем автомобиль
+            consumption = cons;
+        }
+        
+        public void show()
+        {
+            Console.WriteLine(space + " ___________");
+            Console.WriteLine(space + "|  |     |  |");
+            Console.WriteLine(space + "|  |     |  |");
+            Console.WriteLine(space + "|__|_____|__|\n");
+            Console.WriteLine(status);
+        }
+
+        public void setStatus()
+        {
+            status = "Машина #" + n + " Температура - " + temp/10
+                + " Осталось топлива - " + fuel;
+        }
+        
+        public void move()
+        {
+           
+
+            if (position < 65 &&// не даем машинам выехать за край экрана
+                fuel >= 0 + consumption &&// не даем машинам ехать без топлива
+                temp <= 1000)//не даем машинам ехать при перегреве
+            {
+                setStatus();
+                fuel -= consumption;
+                temp += rand.Next(1, 20);// случайным образом подогреваем все автомобили
+                if (n == 2) temp += 10;// специально подогреваем третью машину чтобы вызвать событие
+                space += " ";
+                position++;
+            }
+            else
+            #region ПОДПИСКА НА СОБИТИЯ
+            {
+                if (onEvent != null) return;
+
+                OK = false;
+
+                if (position >= 64)
+                {
+                    onEvent = Events.OnEvent;
+                    onEvent += Events.OnFinish;
+                    onEvent(this, ref status);
+                }
+               
+                if(fuel <= 0 + consumption)
+                {
+                    onEvent = Events.OnEvent;
+                    onEvent += Events.OnFuelEnd;
+                    onEvent(this, ref status);
+                }
+
+                if (temp >= 1000)
+                {
+                    onEvent = Events.OnEvent;
+                    onEvent += Events.OnOverheat;
+                    onEvent(this, ref status);
+                }
+            }
+            #endregion
+        }
+    }
+
+    class Track
+    {
+        static Car[] cars = { new Car(5), new Car(14), new Car(7) };
+
+        public static void show()
+        {
+            Console.Clear();
+
+            foreach (Car a in cars)
+            {
+                a.show();               
+            }
+        }
+
+        public static void race()
+        {
+            while (true)
+            {
+                for(int i = 0; i <= cars.Length; ++i)// когда у всех машин сработали события завершаем гонку
+                {
+                    if (i == cars.Length) return;
+                    if (cars[i].OK) break;
+                }
+
+                Thread.Sleep(100);
+
+                foreach (Car car in cars)
+                    car.move();               
+
+                show();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Track.race();           
+           
+        }
+    }
+}
---
+++ CSharp/HW6/HW6/Program.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace ConsoleApplication3
+{
+    enum Value { J = 11, Q, K, A };
+    enum Suit { Hearts = '♥', Diamonds, Clubs, Spades }; // ♦♣♠
+
+
+    class Card : IComparable
+    {
+
+        public int CompareTo(object card)
+        {
+            if (this.value < ((Card)card).value) return -1;
+            if (this.value > ((Card)card).value ||
+            card == null) return 1;
+            return 0;
+        }
+
+        Value value;
+        Suit suit;
+
+        public Card(Value value, Suit suit)
+        {
+            if (value < (Value)6) value = (Value)6;
+            if (value > Value.A) value = Value.A;
+            this.value = value;
+            this.suit = suit;
+        }
+
+        public void show()
+        {
+            Console.Write(value + " ");
+            if (value != (Value)10) Console.Write(' ');
+            Console.Write((char)suit + "\n");
+
+        }
+
+        public override string ToString()
+        {
+            return value.ToString();
+        }
+
+    }
+
+    static class Deck
+    {
+        public static List<Card> cards = new List<Card>();
+        static Deck()
+        {
+            for (int i = 3; i <= 6; ++i)
+                for (int j = 6; j <= (int)Value.A; ++j)
+                    cards.Add(new Card((Value)j, (Suit)i));
+
+
+            shuffle();
+
+
+            // cards.Add(new Card((Value)6, Suit.Diamonds));
+            // cards.Add(new Card(Value.A, Suit.Diamonds));
+
+
+        }
+
+        public static void ReConstruct()
+        {
+            cards.Clear();
+
+            for (int i = 3; i <= 6; ++i)
+                for (int j = 6; j <= (int)Value.A; ++j)
+                    cards.Add(new Card((Value)j, (Suit)i));
+
+
+            shuffle();
+        }
+
+        static void shuffle()
+        {
+            Random rand = new Random(DateTime.Now.Millisecond);
+
+            for (int i = 0; i < 100000; ++i)
+            {
+                int a = rand.Next(0, 36), b = rand.Next(0, 35);
+
+                Card temp = cards[a];
+                cards[a] = cards[b];
+                cards[b] = temp;
+            }
+        }
+        public static void show()
+        {
+            foreach (Card a in cards)
+                a.show();
+        }
+    }
+    
+    class Player
+    {
+        public int n;
+
+        public List<Card> cards;
+
+        public Player(int n, int cardsNumber)
+        {
+            cards = new List<Card>();
+            
+            this.n = n;
+
+            for (int i = 0; i < cardsNumber; ++i)
+            {
+                cards.Add(Deck.cards[0]);
+                Deck.cards.RemoveAt(0);
+            }
+        }
+
+        public void showStatus()
+        {
+            Console.Write("Игрок №" + n /*+ "\nКарты:\n"*/);
+            Console.Write( " Имеет " + cards.Count + " карт\n");
+
+            foreach (Card card in cards)
+                card.show();   
+
+        }
+
+        public void Move()
+        {
+            Game.desk.Add(cards[0]);
+            cards.RemoveAt(0);
+        }
+
+        public void TakeDesk()
+        {
+            foreach (Card card in Game.desk)
+                cards.Add(card);
+
+            Game.desk.Clear();
+        }
+
+
+
+    }
+
+    static class Game
+    {
+        public static List<Player> players;
+        public static List<Card> desk;
+
+        static Game()
+        {
+
+            players = new List<Player>();
+            desk = new List<Card>();
+        }
+
+
+         static void SetPlayers(int n)
+        {
+            if (36 % n != 0)
+            {
+                Console.WriteLine("Невеное количество игроков, \nигроки должны полностью забрать колоду из 36-ти карт");
+                return;
+            }
+
+
+            for (int i = 0; i < n; ++i)
+                players.Add(new Player(i + 1, 36 / n));
+        }
+
+         static void Start()
+        {           
+
+            if (players.Count == 0)
+            {
+                Console.WriteLine("Для начала игры установите количество игроков");
+                return;
+            }
+
+            if (players.Count == 1)
+            {
+                Console.WriteLine("В игре только один игрок. Он победил.");
+                return;
+            }                           
+            
+
+        }
+
+         static void Playing()
+    {
+        while (true)
+        {
+           // Console.Clear();
+
+
+            foreach (Player player in players) player.Move();
+
+           // foreach (Card card in desk) card.show();
+           
+            int maxCard = desk.IndexOf(desk.Max());
+
+            players[maxCard].TakeDesk();
+
+            for (int i = 0; i < players.Count; ++i)
+            {
+                if (players[i].cards.Count == 0)// убираем игроков без карт
+                {
+                    players.Remove(players[i]);
+                    i--;
+                    continue;
+                }
+
+                if (players[i].cards.Count == 36)
+                {
+                   // Console.WriteLine("Победил игрок №" + players[i].n);
+                    Program.finiteGames++;
+                    return;
+                }
+            }
+
+
+
+
+            Program.i++;
+            if (Program.i > 1000)
+            {
+                Program.i = 0;
+                Program.infiniteGames++;
+                return;
+            }
+
+            //Console.ReadKey(true);
+        }
+    }
+
+         static void GameOver()
+        {
+            Deck.ReConstruct();
+            players.Clear();          
+        }
+
+         public static void Play(int numberOfPlayers)
+        {
+            SetPlayers(numberOfPlayers);
+            Start();
+            Playing();
+            GameOver();
+        }
+    }
+
+    class Program
+    {
+
+        public static int i = 0;
+        public static int infiniteGames = 0;
+        public static int finiteGames = 0;
+
+        static void TestGame()
+        {
+            int[] playersN = { 2, 3, 4, 6, 9, 12, 18, 36 };
+
+
+            for (int j = 0; j < playersN.Count(); ++j)
+            {
+
+
+                for (int i = 0; i < 1000; ++i)
+                {
+                    Game.Play(playersN[j]);
+                }
+
+                Console.WriteLine("Для " + playersN[j] + " игроков:");
+                Console.Write("Бесконечных игр - " + infiniteGames);
+                Console.WriteLine(" Конечных игр - " + finiteGames + "\n");
+
+                infiniteGames = 0;
+                finiteGames = 0;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+
+
+            TestGame();
+
+        }
+
+
+    }
+}
