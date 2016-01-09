7783554c9bfadb4d0552964ffa75481bbf66c4a8
Vladas Zakrevskis <146100@gmail.com>
2016-01-09 22:33:52 +0300

    ...

---
+++ CSharp/HW9/ClassLib/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    [Serializable]
+    public class PC
+    {
+        string manufacturer;
+        public string Manufacturer
+        {
+            set
+            {
+                manufacturer = value;
+            }
+            get
+            {
+                return manufacturer;
+            }
+        }
+        int serialNumber;
+        public int SerialNumber
+        {
+            set
+            {
+                serialNumber = value;
+            }
+            get
+            {
+                return serialNumber;
+            }
+        }
+        int hdd;
+        public int HDD
+        {
+            set
+            {
+                hdd = value;
+            }
+            get
+            {
+                return hdd;
+            }
+        }
+
+        public PC()
+        {
+            manufacturer = "Asus";
+            serialNumber = 88888888;
+            hdd = 500;
+        }
+
+        public PC(string manufacturer, int serialNumber, int hdd)
+        {
+            this.manufacturer = manufacturer;
+            this.serialNumber = serialNumber;
+            this.hdd = hdd;
+        }
+
+        public void turnOn()
+        {
+            Console.WriteLine(manufacturer + " " + serialNumber + " is ON");
+        }
+        public void turnOff()
+        {
+            Console.WriteLine(manufacturer + " " + serialNumber + " is OFF");
+        }
+        public void reboot()
+        {
+            Console.WriteLine(manufacturer + " " + serialNumber + " is rebooted");
+        }
+
+
+    }
+}
---
+++ CSharp/HW9/SerializeConsolApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using ClassLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SerializeConsolApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<PC> pcStorage = new List<PC>();
+
+            pcStorage.AddRange(new PC[] {
+                new PC(),
+                new PC("Samsung", 123456789, 1000),
+                new PC("Lenovo", 987654321, 320),
+                new PC("Apple", 46518928, 550)});
+            foreach (PC pc in pcStorage)
+            {
+                pc.turnOn();
+            }
+
+            using (FileStream file = new FileStream("pcStorageData.txt", FileMode.OpenOrCreate))
+            {
+                BinaryFormatter binFormat = new BinaryFormatter();
+                binFormat.Serialize(file, pcStorage);
+            }
+
+
+        }
+    }
+}
