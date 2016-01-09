f88e9137c5b88607967f4b56489d458cc22b809f
Vladas Zakrevskis <146100@gmail.com>
2016-01-09 23:28:53 +0300

    ...

---
+++ CSharp/HW9/DeserializeConsolApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLib;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace DeserializeConsolApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<PC> openStorage = new List<PC>();
+
+            using (FileStream file = new FileStream(@"C:\pcStorageData.txt", FileMode.Open))
+            {
+                BinaryFormatter binFormat = new BinaryFormatter();
+                openStorage = (List<PC>)binFormat.Deserialize(file);
+            }
+
+            foreach (PC pc in openStorage)
+                pc.turnOn();
+        }
+    }
+}
--- CSharp/HW9/SerializeConsolApp/Program.cs
+++ CSharp/HW9/SerializeConsolApp/Program.cs
@@ -18,12 +18,13 @@ namespace SerializeConsolApp
                 new PC("Samsung", 123456789, 1000),
                 new PC("Lenovo", 987654321, 320),
                 new PC("Apple", 46518928, 550)});
+
             foreach (PC pc in pcStorage)
             {
                 pc.turnOn();
             }
 
-            using (FileStream file = new FileStream("pcStorageData.txt", FileMode.OpenOrCreate))
+            using (FileStream file = new FileStream(@"C:\pcStorageData.txt", FileMode.OpenOrCreate))
             {
                 BinaryFormatter binFormat = new BinaryFormatter();
                 binFormat.Serialize(file, pcStorage);
