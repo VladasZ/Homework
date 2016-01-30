bfa006e58952511b35d1aa39ecfdb03f83b4c4a8
Vladas Zakrevskis <146100@gmail.com>
2016-01-30 20:36:26 +0300

    UIScrollView

--- BelHard/Homework/UIScrollVIew/UIScrollVIew/ViewController.m
+++ BelHard/Homework/UIScrollVIew/UIScrollVIew/ViewController.m
@@ -23,7 +23,8 @@
 @implementation ViewController
 
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     
     _screenSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height);
@@ -35,26 +36,43 @@
     
     [self.view addSubview:_scrollView];
     
+    for (NSUInteger i = 0; i < 100; i++) {
+        
+        UIScrollView *subScroll = [[UIScrollView alloc] initWithFrame:CGRectMake(_screenSize.width * i, 0, _screenSize.width, _screenSize.height)];
+        
+        [self add:100 turtlesOnScrollWiew:subScroll];
+        
+        
+        [_scrollView addSubview:subScroll];
+        
+        [_scrollView setContentSize:CGSizeMake(_screenSize.width * (i + 1), _screenSize.height)];
+        
+        
+    }
+    
+    
+    
+}
+
+- (void)add:(NSUInteger)turtles turtlesOnScrollWiew:(UIScrollView *)scrollView
+{
     CGRect imageRect = CGRectMake(10, 10, _screenSize.width - 20, _screenSize.height - 20);
     
     
     
     
-
-    for (NSUInteger i = 0; i < 10; i++) {
+    
+    for (NSUInteger i = 0; i < turtles; i++) {
         
         UIImageView *imageView = [[UIImageView alloc] initWithFrame:imageRect];
         [imageView setImage:_image];
         
-        [_scrollView addSubview:imageView];
+        [scrollView addSubview:imageView];
         
-        _scrollView.contentSize = CGSizeMake(_screenSize.width, _screenSize.height * (i + 1));
+        scrollView.contentSize = CGSizeMake(_screenSize.width, _screenSize.height * (i + 1));
         
         imageRect.origin.y += _screenSize.height;
     }
-   
-    
 }
-
 
 @end
