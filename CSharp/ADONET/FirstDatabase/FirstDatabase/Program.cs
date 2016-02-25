013adad70e1f8495a58c55b69e20b50cf9d98e22
Vladas Zakrevskis <146100@gmail.com>
2016-02-25 23:29:32 +0300

    ...

---
+++ CSharp/ADONET/FirstDatabase/FirstDatabase/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C3EasyRandom;
+
+
+namespace FirstDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            User newUser = new User("peter", 12);
+
+            Console.WriteLine(newUser.name);
+        }
+    }
+}
