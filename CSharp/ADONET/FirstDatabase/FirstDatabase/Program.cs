e72cab65fbc09733c9ca5fd77846f718c08facaf
Vladas Zakrevskis <146100@gmail.com>
2016-02-26 00:28:22 +0300

    ...

--- CSharp/ADONET/FirstDatabase/FirstDatabase/Program.cs
+++ CSharp/ADONET/FirstDatabase/FirstDatabase/Program.cs
@@ -3,19 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using C3EasyRandom;
+using System.Data.OleDb;
 
 
 namespace FirstDatabase
 {
     class Program
     {
+        static OleDbConnection connection = new OleDbConnection();
+
+        static Program()
+        {
+            connection.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Vladas\Desktop\WinForms\Day1 ADO\med.mdb";
+
+            connection.Open();
+        }
+
+        static OleDbDataReader readerForCommand(string commandString)
+        {
+            OleDbCommand command = new OleDbCommand(commandString, connection);
+
+            return command.ExecuteReader();
+        }
+
+        static void showAllPatients()
+        {
+            OleDbDataReader reader = readerForCommand("SELECT * FROM Patients");
+
+
+            Console.WriteLine("Все пациенты:");
+
+            while (reader.Read() != false)
+            {
+
+                Console.WriteLine("{0,-10} {1,-10} {2,-10}", reader["patientID"], 
+                                                            reader["patientName"], reader["patientFirstName"]);
+
+            }
+        }
+
+
         static void Main(string[] args)
         {
-
-            User newUser = new User("peter", 12);
-
-            Console.WriteLine(newUser.name);
+            showAllPatients();
         }
     }
 }
