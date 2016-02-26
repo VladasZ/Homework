174ceb2ab57daed0ccb979b3f7bb2a27c4c04648
Vladas Zakrevskis <146100@gmail.com>
2016-02-27 00:48:29 +0300

    ...

--- CSharp/ADONET/FirstDatabase/FirstDatabase/Program.cs
+++ CSharp/ADONET/FirstDatabase/FirstDatabase/Program.cs
@@ -19,33 +19,61 @@ namespace FirstDatabase
             connection.Open();
         }
 
-        static OleDbDataReader readerForCommand(string commandString)
+        static OleDbCommand commandForQuery(string commandString)
         {
-            OleDbCommand command = new OleDbCommand(commandString, connection);
-
-            return command.ExecuteReader();
+              return new OleDbCommand(commandString, connection);
         }
 
         static void showAllPatients()
         {
-            OleDbDataReader reader = readerForCommand("SELECT * FROM Patients");
-
+            OleDbDataReader reader = commandForQuery("SELECT * FROM Patients").ExecuteReader();
 
             Console.WriteLine("Все пациенты:");
 
             while (reader.Read() != false)
             {
-
-                Console.WriteLine("{0,-10} {1,-10} {2,-10}", reader["patientID"], 
-                                                            reader["patientName"], reader["patientFirstName"]);
-
+                Console.WriteLine("{0,-2}{1,-10}{2,-10}{3, -10}", 
+                                  reader["patientID"],
+                                  reader["patientName"],
+                                  reader["patientFirstName"],
+                                  reader["patientBirthDate"]);
             }
         }
+        
+        static void showDoctorsCount()
+        {
+            int doctorsCount = (int)commandForQuery("SELECT COUNT(doctorID) FROM Doctors").ExecuteScalar();
 
+            Console.WriteLine("\nКоличество докторов в больнице: {0}", doctorsCount);
+
+        }
+
+        static void mostExpenssiveVisit()
+        {
+
+            OleDbDataReader reader = 
+                commandForQuery("SELECT* FROM VisitCosts WHERE visitCostValue = " + 
+                                "(SELECT max(visitCostValue) FROM VisitCosts)").ExecuteReader();
+
+            Console.WriteLine("\nСамый дорогой визит:");
+
+
+            while (reader.Read() != false)
+            {
+                Console.WriteLine("{0,-2}{1,-10}{2,-10}{3, -10}",
+                                  reader["visitCostID"],
+                                  reader["visitCostFrom"],
+                                  reader["visitCostTill"],
+                                  reader["visitCostValue"]);
+            }
+
+        }
 
         static void Main(string[] args)
         {
             showAllPatients();
+            showDoctorsCount();
+            mostExpenssiveVisit();
         }
     }
 }
